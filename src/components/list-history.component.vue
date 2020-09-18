d8a5620e4e31b95c363f7acaaff9baaf3d55c4eb
nattaweekan <nattaweekan.w@gmail.com>
2020-09-19 02:00:58 +0700

    :bug: :zap: fixed

--- src/components/list-history.component.vue
+++ src/components/list-history.component.vue
@@ -10,22 +10,11 @@
           v-model="search"
         />
       </div>
-      <button
-        class="search-relative search-card"
-        @click="openCombobox = !openCombobox"
-      >
+      <button class="search-relative search-card" @click="openCombobox = !openCombobox">
         <span>{{ isChecked }}</span>
-        <div
-          class="search-combobox search-combobox-icon"
-        >
-          <svg
-            style="width:24px;height:24px"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fill="currentColor"
-              d="M7,10L12,15L17,10H7Z"
-            />
+        <div class="search-combobox search-combobox-icon">
+          <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+            <path fill="currentColor" d="M7,10L12,15L17,10H7Z" />
           </svg>
         </div>
         <div :class="comboboxList()">
@@ -34,57 +23,44 @@
             @click="onClickCombobox(item)"
             v-for="item in itemsList"
             :key="item"
-          >
-            {{ item }}
-          </button>
+          >{{ item }}</button>
         </div>
       </button>
     </div>
     <div class="history-card">
-      <div
-        v-if="dataHistorys.length === 0"
-        class="history-msg"
-      >
+      <div v-if="dataHistorys.length === 0" class="history-msg">
         <h3>No data available</h3>
       </div>
-      <div
-        v-else
-        v-for="(item, index) in dataHistorys"
-        :key="index"
-        class="history-item"
-      >
+      <div v-else v-for="(item, index) in dataHistorys" :key="index" class="history-item">
         <h3>Calculator {{ item.name }}</h3>
-        <h3 class="time">
-          {{ item.date }}
-        </h3>
-        <h2 class="history-item-merge">
-          {{ item.result }}
-        </h2>
+        <h3 class="time">{{ item.date }}</h3>
+        <h2 class="history-item-merge">{{ item.result }}</h2>
         <hr class="dividers history-item-merge" />
-        <h3 class="history-item-merge">
-          {{ item.data }}
-        </h3>
+        <h3 class="history-item-merge">{{ item.data }}</h3>
       </div>
     </div>
-    <button class="btn-clear" @click="clear">
-      Clear
-    </button>
+    <button class="btn-clear" @click="clear">Clear</button>
   </div>
 </template>
 
 <script>
 export default {
-  props: {
-    historys: Array,
-  },
   data: () => ({
     openCombobox: false,
     isChecked: "All",
     itemsList: ["A", "B", "All"],
     dataHistorys: [],
     search: "",
+    historys: []
   }),
   mounted() {
+    if (localStorage.getItem("results")) {
+      this.historys = JSON.parse(localStorage.results);
+    }
+    window.addEventListener("results-localstorage-changed", event => {
+      this.historys = JSON.parse(localStorage.getItem("results"));
+    });
+
     this.onClickCombobox("All");
   },
   methods: {
@@ -92,64 +68,47 @@ export default {
       return [
         "combobox-item",
         {
-          "combobox-checked":
-            this.isChecked === val,
-        },
+          "combobox-checked": this.isChecked === val
+        }
       ];
     },
     comboboxList() {
       return [
         "combobox",
         {
-          "combobox-is-hidden": !this
-            .openCombobox,
-        },
+          "combobox-is-hidden": !this.openCombobox
+        }
       ];
     },
     clear() {
-      this.$emit("RESULT", false);
+      this.$emit("CLEAR", true);
     },
     onClickCombobox(val) {
       this.dataHistorys = [];
       this.isChecked = val;
-      for (
-        let i = 0;
-        i < this.historys.length;
-        i += 1
-      ) {
-        if (this.isChecked === "All") {
-          this.dataHistorys.push(
-            this.historys[i]
-          );
-        } else if (
-          this.historys[i].name === this.isChecked
-        ) {
-          this.dataHistorys.push(
-            this.historys[i]
-          );
-        }
+      for (let i = 0; i < this.historys.length; i += 1) {
+        if (this.isChecked === "All") this.dataHistorys.push(this.historys[i]);
+        else if (this.historys[i].name === this.isChecked)
+          this.dataHistorys.push(this.historys[i]);
       }
     },
     onSearch(val) {
       this.dataHistorys = [];
-      const data = this.historys.filter(function(
-        item
-      ) {
-        const res =
-          item.result.includes(val) ||
-          item.date.includes(val);
+      const data = this.historys.filter(function(item) {
+        const res = item.result.includes(val) || item.date.includes(val);
         return res;
       });
       this.dataHistorys = data;
-    },
+    }
   },
   watch: {
-    historys() {
-      this.onClickCombobox(this.isChecked);
+    historys(val) {
+      if (val) this.onClickCombobox(this.isChecked);
+      else this.dataHistorys = [];
     },
     search(val) {
       this.onSearch(val);
-    },
-  },
+    }
+  }
 };
 </script>
