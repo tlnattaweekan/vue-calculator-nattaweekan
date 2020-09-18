ee1b98cd9aee2928234ec781c2f502ac10d330a1
nattaweekan <nattaweekan.w@gmail.com>
2020-09-18 17:00:50 +0700

    :zap: :zap: responsive ui

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
         :nameCalculator="'B'"
       />
       <list-history-component
+        class="display-history"
         @RESULT="getResultHistory"
         :historys="history"
       />
@@ -28,14 +29,8 @@ export default {
   }),
   methods: {
     getResultHistory(val) {
-      if (!val) {
-        this.history = [];
-      } else {
-        console.log("vald", val);
-        console.log(this.history.length);
-        this.history.push(val);
-        console.log("val", this.history);
-      }
+      if (!val) this.history = [];
+      else this.history.push(val);
     },
   },
   components: {
@@ -46,36 +41,5 @@ export default {
 </script>
 
 <style lang="scss">
-.app-bar {
-  height: 3.5rem;
-  background-color: white;
-  font-size: 1.4rem;
-  color: gray;
-  box-shadow: 0 8px 10px -7px #e0e0e0;
-  display: flex;
-  padding: 0 1rem;
-  align-items: center;
-}
-
-.layout-content {
-  display: grid;
-  grid-template-columns: 28% 28% 44%;
-  justify-items: center;
-  margin-top: 3rem;
-  @media only screen and (min-width: 1500px) {
-    padding: 0 6rem;
-  }
-  @media only screen and (min-width: 1600px) {
-    padding: 0 10rem;
-  }
-  @media only screen and (min-width: 1800px) {
-    padding: 0 16rem;
-  }
-}
-#app {
-  background-color: #f7f7f7;
-  height: 100vh;
-}
-
 @import "./core/styles/main.scss";
 </style>
